6a5105273fad96ea1fa11f3487b38a829a983e29
Vivek Verma <vverma240420@gmail.com>
2023-07-09 14:40:18 +0530

    Added Duplicate count progrma

---
+++ PracticeProblem4B/DuplicateCount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem4B
+{
+    internal class DuplicateCount
+    {
+        public int CountDuplicate(int[] array)
+        {
+            Dictionary<int, int> elementCount = new Dictionary<int, int>();
+            foreach (int num in array)
+            {
+                if (elementCount.ContainsKey(num))
+                {
+                    elementCount[num]++;
+                }
+                else
+                {
+                    elementCount[num] = 1;
+                }
+            }
+            int duplicateCount = 0;
+
+
+            foreach ( int count in elementCount.Values) 
+            {
+                if(count >1) 
+                {
+                    duplicateCount++;
+                }
+            }
+            return duplicateCount;
+        }        
+    }
+}
---
+++ PracticeProblem4B/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PracticeProblem4B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 3, 4, 5, 6, 4, 3, 9, 6, 7 };
+
+            DuplicateCount DC = new DuplicateCount();
+            int duplicateCount = DC.CountDuplicate(array);
+
+
+            Console.WriteLine("Total number of duplicate elements: " + duplicateCount);
+        }
+    }
+}
